d61cb7f36472ecebf35372c1dc4d1259f2dafa43
vasishakd <vasishak.d@gmail.com>
2019-11-22 15:59:51 +0500

    fix headers

--- resources/js/tool.js
+++ resources/js/tool.js
@@ -1,5 +1,7 @@
 import ResourceTableRow from './components/ResourceTableRow';
+import ResourceTable from './components/ResourceTable';
 
 Nova.booting((Vue, router, store) => {
     Vue.component('resource-table-row', ResourceTableRow);
+    Vue.component('resource-table', ResourceTable);
 });
